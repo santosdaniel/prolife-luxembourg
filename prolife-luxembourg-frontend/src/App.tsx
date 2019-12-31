61ba5b2ce234b4bc14121e1584000ecfba81c498
Daniel Ferreira <danielferreira@ua.pt>
2019-12-31 13:58:37 +0000

    Add support for typescript

---
+++ prolife-luxembourg-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+      </div>
+    );
+  }
+}
