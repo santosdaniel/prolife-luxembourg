e9c7044e65cb0187df8f2dc966f4d3da855b881e
Daniel Ferreira <danielferreira@ua.pt>
2020-01-12 20:08:12 +0100

    Automatic detection of languages

--- prolife-luxembourg-frontend/src/index.tsx
+++ prolife-luxembourg-frontend/src/index.tsx
@@ -5,20 +5,31 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { I18nextProvider } from 'react-i18next';
 import i18next from 'i18next';
+import { universalLanguageDetect } from '@unly/universal-language-detector';
 import frTranslations from "./translations/fr.json";
 import enTranslations from "./translations/en.json";
+
+const languageOfBrowser = universalLanguageDetect({
+    supportedLanguages: ["en", "fr"], // Whitelist of supported languages, will be used to filter out languages that aren't supported
+    fallbackLanguage: "en", // Fallback language in case the user's language cannot be resolved
+    errorHandler: (error) => { // Optional - Use you own logger here, Sentry, etc.
+      console.log('Was not possible to detect the language of the user error handler:');
+      console.error(error);
+    },
+  });
+
 
 i18next.init({
     react: {
         useSuspense: false
     },
     interpolation: { escapeValue: false },  // React already does escaping
-    lng: 'en',                              // language to use
+    lng: languageOfBrowser,                              // language to use
         resources: {
             en: {
                 'translations': enTranslations //name of the property registred in the app.tsx
             },
-            de: {
+            fr: {
                 'translations': frTranslations
             },
         },
