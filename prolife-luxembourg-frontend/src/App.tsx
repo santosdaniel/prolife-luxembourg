26effb88afea9d47c25c7fdabecc620cef89dd13
Daniel Ferreira <danielferreira@ua.pt>
2020-01-12 19:56:22 +0100

    Add translation library.

--- prolife-luxembourg-frontend/src/App.tsx
+++ prolife-luxembourg-frontend/src/App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { withTranslation, Trans, WithTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
-export default class App extends React.Component {
+class App extends React.Component<WithTranslation> {
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>this is text component of test</p>
+          <h1 className="App-title">
+                { this.props.t('welcome.title', { framework: "react-i18next" }) }
+            </h1>
+          <Trans i18nKey='welcome.intro'>
+          <h1 className="App-title">Welcome to React</h1>
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </Trans>
+            <button onClick={() => i18next.changeLanguage('de')}>de</button>
+                <button onClick={() => i18next.changeLanguage('en')}>en</button>
         </header>
       </div>
     );
   }
 }
+
+export default withTranslation("translations")(App);
--- prolife-luxembourg-frontend/src/index.tsx
+++ prolife-luxembourg-frontend/src/index.tsx
@@ -3,8 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { I18nextProvider } from 'react-i18next';
+import i18next from 'i18next';
+import frTranslations from "./translations/fr.json";
+import enTranslations from "./translations/en.json";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+i18next.init({
+    react: {
+        useSuspense: false
+    },
+    interpolation: { escapeValue: false },  // React already does escaping
+    lng: 'en',                              // language to use
+        resources: {
+            en: {
+                'translations': enTranslations //name of the property registred in the app.tsx
+            },
+            de: {
+                'translations': frTranslations
+            },
+        },
+});
+
+const rootComponent = document.getElementById('root');
+ReactDOM.render(
+    <I18nextProvider i18n={i18next}>
+        <App />
+    </I18nextProvider>
+    , rootComponent
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
